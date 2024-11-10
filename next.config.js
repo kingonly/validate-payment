9c8082ee92c8482dc64775e324555c050ec6dfc4
Roy Sheinfeld <kingonly@gmail.com>
2024-11-10 18:14:59 +0200

    Update next.config.js with additional polyfills and webpack config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,21 @@
 const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // Fallbacks for node modules
       config.resolve.fallback = {
         ...config.resolve.fallback,
         stream: require.resolve('stream-browserify'),
         crypto: require.resolve('crypto-browserify'),
         buffer: require.resolve('buffer/'),
+        util: require.resolve('util/'),
       };
+
+      // Add plugins for buffer
+      config.plugins.push(
+        new config.webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+        })
+      );
     }
     return config;
   },
